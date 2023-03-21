d62b5c2ff0e4f686312210927a671f551dae7ed1
recepkabakci <103920618+recepkabakci@users.noreply.github.com>
2023-03-21 17:42:36 +0100

    coderbyte

---
+++ src/CoderByte.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CoderByte {
+    public static int FirstFactorial(int num) {
+
+        return num;
+    }
+
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        System.out.print(FirstFactorial(Integer.parseInt(s.nextLine())));
+    }
+
+}
+}
