34edfddee66b86469b34f428cf9d9e42278f0369
recepkabakci <103920618+recepkabakci@users.noreply.github.com>
2023-03-22 14:25:01 +0100

    coderbyte factoriel

--- src/CoderByte.java
+++ src/CoderByte.java
@@ -1,16 +1,14 @@
-import java.util.Scanner;
 
 public class CoderByte {
-    public static int FirstFactorial(int num) {
-
-        return num;
+    public static void FirstFactorial(int num) {
+        int sum =1;
+        for(int i=num;i>0;i--) {
+            sum=sum*i;
+        }
     }
 
-    public static void main (String[] args) {
-        // keep this function call here
-        Scanner s = new Scanner(System.in);
-        System.out.print(FirstFactorial(Integer.parseInt(s.nextLine())));
+    public static void main(String[] args) {
+        FirstFactorial(5);
     }
+}
 
-}
-}
