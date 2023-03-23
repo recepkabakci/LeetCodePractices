489eff405254225976978bcc06f93bfd2caad67c
recepkabakci <103920618+recepkabakci@users.noreply.github.com>
2023-03-23 13:33:24 +0100

    reverse string easily with stringbuilder

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,8 +1,5 @@
 public class ReverseString {
     public static String FirstReverse(String str) {
-        StringBuilder input = new StringBuilder();
-        input.append(str).reverse();
-        String singleString = input.toString();
-        return singleString;
+        return new StringBuilder(str).reverse().toString();
     }
 }
