d9740d7af47ff8cc2a17e7e1e391dece8f2d87d2
recepkabakci <103920618+recepkabakci@users.noreply.github.com>
2023-03-23 13:04:39 +0100

    regex validation

--- src/CodelandUserNameValidation.java
+++ src/CodelandUserNameValidation.java
@@ -2,27 +2,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CodelandUserNameValidation {
-    public static boolean CodelandUsernameValidation(String str) {
-        // Regex to check valid username.
-        String regex = "^[A-Za-z]\\w{5,29}$";
-
-        // Compile the ReGex
-        Pattern p = Pattern.compile(regex);
-
-        // If the username is empty
-        // return false
-        if (str == null) {
-            return false;
-        }
-
-        // Pattern class contains matcher() method
-        // to find matching between given username
-        // and regular expression.
-        Matcher m = p.matcher(str);
-
-        // Return if the username
-        // matched the ReGex
-        return m.matches();
-
+    public static String CodelandUsernameValidation(String str) {
+            Pattern p = Pattern.compile("[a-zA-Z][a-zA-Z0-9_]{2,23}[^_]");
+            Matcher matcher = p.matcher(str);
+            return matcher.matches() ? "true" : "false";
     }
 }
