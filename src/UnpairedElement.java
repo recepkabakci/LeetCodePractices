0a9b7d02c5e0665c33b48e37ee83177e888a360e
recepkabakci <103920618+recepkabakci@users.noreply.github.com>
2023-03-24 17:15:14 +0100

    unpaired element is found

--- src/UnpairedElement.java
+++ src/UnpairedElement.java
@@ -1,11 +1,22 @@
 public class UnpairedElement {
     public int solution(int[] A) {
-        for(int i=0;i<A.length-1;i++){
-            for(int j=1;j<A.length;j++){
-                if(A[i]==A[j]){
-
+        int unpairedNUmber=0;
+        for(int i=0;i<A.length;i++){
+            int count=0;
+            int num=A[i];
+            for(int j=0;j<A.length;j++){
+                if(num==A[j]){
+                    count++;
                 }
             }
+            if(count<2){
+                unpairedNUmber=num;
+            }
         }
+
+
+
+        return unpairedNUmber;
     }
+
 }
